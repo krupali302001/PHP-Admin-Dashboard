f9b49c38544bfef8f70f2b398b9dc1338be37e85
krupali302001 <krupalip008@gmail.com>
2024-01-29 11:19:36 +0530

    day 8

--- pages/pdf.php
+++ pages/pdf.php
@@ -1,16 +1,46 @@
 <?php
 include('../includes/dbcon.php');
-$sql="SELECT * FROM userData WHERE ID='31'";
-$result=sqlsrv_query($Con, $sql);
-$row= sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
+$sql = "SELECT * FROM userData WHERE ID='31'";
+$result = sqlsrv_query($Con, $sql);
+$row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PDF DEMO</title>
+    <style>
+        .header {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+
+        .name {
+            display: flex;
+            flex-direction: row;
+        }
+
+        .firstname {
+            font-size: 36px;
+            font-family: monospace;
+        }
+
+        .lastname {
+            font-size: 36px;
+            font-family: serif;
+        }
+
+        .contactinfo {
+            display: block;
+            float: right;
+            font-size: 18px;
+        }
+    </style>
 </head>
+
 <body>
     <table>
         <thead>
@@ -27,32 +57,35 @@ $row= sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
             </tr>
         </thead>
         <tbody>
-        <tr>
+            <tr>
                 <td><img src="../pages/profile/<?php echo $row['profile_image']; ?>" alt="profile_image" width="40px" height="40px"></td>
                 <td class="empid"><?php echo $row['empid'] ?></td>
                 <td class="firstName"><?php echo $row['firstName'] ?></td>
-                <td class="lastName"><?php echo $row['lastName'] ?></td>    
+                <td class="lastName"><?php echo $row['lastName'] ?></td>
                 <td class="contact_number"><?php echo $row['contact_number'] ?></td>
                 <td class="email"><?php echo $row['email'] ?></td>
                 <td class="department"><?php echo $row['department'] ?></td>
                 <td class="designation"><?php echo $row['designation'] ?></td>
-                <td class="join_date"><?php echo $row['join_date']->format('Y-m-d')?></td>
-                </tr>
+                <td class="join_date"><?php echo $row['join_date']->format('Y-m-d') ?></td>
+            </tr>
         </tbody>
 
 </body>
-<!-- Name -->
-<span>
-   <?php echo $row['firstName'] ?>
-   <?php echo $row['lastName'] ?>
-</span>
-<br>
-<!-- Contact Information 
-     Emai
-     Number-->
-<span>
-<?php echo $row['contact_number'] ?><br>
-<?php echo $row['email'] ?>
-</span>     
+<div class="header">
+    <div class="name">
+        <img class="" src="../pages/profile/<?php echo $row['profile_image']; ?>" alt="profile_image" width="150px" height="150px">
+        <!-- Name -->
+        <h3 class="firstname"><?php echo $row['firstName'] ?></h3>
+        <br>
+        <h3 class="lastname"><?php echo $row['lastName'] ?></h3>
+
+    </div>
+    <div class="contactinfo">
+        <!-- Contact Information Emai Number-->
+        <?php echo $row['contact_number'] ?><br>
+        <?php echo $row['email'] ?>
+
+    </div>
+</div>
 
 </html>
